8e05876898bcc14a6b27611b56ea0c6b2bace566
joy-y-cheng <joycheng@ucla.edu>
2024-01-07 01:48:49 -0800

    Add all files

---
+++ eleventy.config.js
@@ -0,0 +1,10 @@
+module.exports = (function(eleventyConfig) {
+
+    return {
+        dir: {
+            input: "src",
+        },
+        templateFormats: ["html","png","svg","jpg","css","js","pdf"]
+    };
+
+});
